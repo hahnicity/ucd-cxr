bdf897488ebd49aef69220e2b86d1443d46ef3f4
Gregory Rehm <grehm87@gmail.com>
2018-07-01 16:08:16 -0700

    add new guan experiments to rerun

--- cxrlib/models/guan_resnet.py
+++ cxrlib/models/guan_resnet.py
@@ -11,10 +11,21 @@ import torchvision
 
 
 class GuanResNet50(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, pretrained=True):
         super(GuanResNet50, self).__init__()
-        self.model = torchvision.models.resnet50(pretrained=True)
-        self.model.fc = torch.nn.Linear(self.model.fc.in_features, 14)
+        self.model = torchvision.models.resnet50(pretrained=pretrained, num_classes=14)
+        self.sig = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.model(x)
+        return self.sig(out)
+
+
+class GuanResNet50Grayscale(torch.nn.Module):
+    def __init__(self, pretrained=True):
+        super(GuanResNet50, self).__init__()
+        self.model = torchvision.models.resnet50(pretrained=pretrained, num_classes=14)
+        self.model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.sig = torch.nn.Sigmoid()
 
     def forward(self, x):
