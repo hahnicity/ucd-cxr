7c3dce38816b9d7b691e1b469a93475ca85af564
Gregory Rehm <grehm87@gmail.com>
2018-07-19 23:33:39 -0700

    add unet experiments

--- cxrlib/init.py
+++ cxrlib/init.py
@@ -11,20 +11,30 @@ Usage:
     model = MyModel()
     model.apply(kaiming_init)
 """
-from torch.nn.init import kaiming_uniform_, xavier_uniform_
+from torch.nn.init import _calculate_fan_in_and_fan_out, kaiming_uniform_, xavier_uniform_
 
 
 def kaiming_init(m):
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find('Conv2d') != -1:
         kaiming_uniform_(m.weight.data)
         if m.bias is not None:
-            kaiming_uniform_(m.bias.data)
+            try:
+                _calculate_fan_in_and_fan_out(m.bias.data)
+            except ValueError:
+                pass
+            else:
+                kaiming_uniform_(m.bias.data)
 
 
 def xavier_init(m):
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find('Conv2d') != -1:
         xavier_uniform_(m.weight.data)
         if m.bias is not None:
-            xavier_uniform_(m.bias.data)
+            try:
+                _calculate_fan_in_and_fan_out(m.bias.data)
+            except ValueError:
+                pass
+            else:
+                xavier_uniform_(m.bias.data)
---
+++ experiments/unet.py
@@ -0,0 +1,93 @@
+import argparse
+import multiprocessing
+import os
+
+import torch
+from torchvision import transforms
+
+from cxrlib.init import kaiming_init, xavier_init
+from cxrlib.models.unet_models import UNet11
+from cxrlib.read_data import get_guan_loaders
+from cxrlib.results import Reporting
+from cxrlib.run import RunModel
+
+
+class UNetRunModel(RunModel):
+    def post_validation_actions(self):
+        epoch_loss = self.reporting.get_meter('validation_epoch_loss').values
+        mean_loss = epoch_loss.mean()
+        self.lr_scheduler.step(mean_loss)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # run related options
+    parser.add_argument('images_path')
+    parser.add_argument('--labels-path', default='/fastdata/chestxray14/labels/')
+    parser.add_argument('--device', default='cuda', choices=['cpu', 'cuda'])
+    parser.add_argument('--results-path', default=os.path.join(os.path.dirname(__file__), 'results'))
+    parser.add_argument('--print-progress', action='store_true')
+    parser.add_argument('--pretrained', action='store_true')
+    parser.add_argument('--load-openi-model', help='specify path to stored openi model')
+    parser.add_argument('--no-validation', action='store_true')
+    # training options
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--batch-size', default=32, type=int)
+    parser.add_argument('--weight-init', choices=['xavier', 'kaiming'], default='kaiming')
+    # model hyperparameters
+    args = parser.parse_args()
+
+    cuda_wrapper = lambda x: x.cuda() if args.device == 'cuda' else x
+    if not args.load_openi_model and not args.pretrained:
+        model = UNet11()
+        if args.weight_init == 'kaiming':
+            model.apply(kaiming_init)
+        elif args.weight_init == 'xavier':
+            model.apply(xavier_init)
+        model = cuda_wrapper(torch.nn.DataParallel(model))
+    elif args.pretrained:
+        model = UNet11(pretrained=True)
+        model.fc = torch.nn.Linear(model.fc.in_features, 14)
+        model = cuda_wrapper(torch.nn.DataParallel(model))
+    else:
+        model = torch.load(args.load_openi_model)
+        model.module.fc = torch.nn.Linear(model.module.fc.in_features, 14)
+        model = cuda_wrapper(model)
+
+    if "preprocessed" in args.images_path:
+        is_preprocessed = True
+    else:
+        is_preprocessed = False
+
+    if args.no_validation:
+        train_loader, test_loader = get_guan_loaders(args.images_path, args.labels_path, args.batch_size, is_preprocessed=is_preprocessed)
+    else:
+        train_loader, valid_loader, test_loader = get_guan_loaders(args.images_path, args.labels_path, args.batch_size, is_preprocessed=is_preprocessed, get_validation_set=True)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=.01, momentum=.9, weight_decay=1e-4)
+    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=10, mode='min')
+    criterion = torch.nn.BCEWithLogitsLoss()
+    reporting = Reporting(args.results_path)
+    reporting.register(model, 'model', False)
+    runner = UNetRunModel(
+        args,
+        model,
+        train_loader,
+        test_loader,
+        optimizer,
+        lr_scheduler,
+        criterion,
+        True if args.device == 'cuda' else False,
+        reporting,
+        validation_loader=valid_loader,
+    )
+    runner.train_multi_epoch(args.epochs)
+    del train_loader
+    del valid_loader
+    torch.cuda.empty_cache()
+    runner.generic_test_epoch()
+    reporting.save_all('unet11-rgb-pretrained-{}'.format(args.pretrained))
+
+
+if __name__ == "__main__":
+    main()
