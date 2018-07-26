918955c97f2efdfae25be0b57906a8c0ebae38a7
Gregory Rehm <grehm87@gmail.com>
2018-07-25 23:47:42 -0700

    add resnet color experiment modifications

--- experiments/resnet_color.py
+++ experiments/resnet_color.py
@@ -9,14 +9,7 @@ from torchvision.models.resnet import resnet50
 from cxrlib.init import kaiming_init, xavier_init
 from cxrlib.read_data import get_guan_loaders
 from cxrlib.results import Reporting
-from cxrlib.run import RunModel
-
-
-class Resnet50RGBRun(RunModel):
-    def post_validation_actions(self):
-        epoch_loss = self.reporting.get_meter('validation_epoch_loss').values
-        mean_loss = epoch_loss.mean()
-        self.lr_scheduler.step(mean_loss)
+from cxrlib.run import RunModelWithAUCAndValLR
 
 
 def main():
@@ -32,17 +25,16 @@ def main():
     parser.add_argument('--no-validation', action='store_true')
     # training options
     parser.add_argument('--epochs', default=50, type=int)
-    parser.add_argument('--batch-size', default=32, type=int)
+    parser.add_argument('--batch-size', default=16, type=int)
     parser.add_argument('--weight-init', choices=['xavier', 'kaiming'], default='xavier')
     # model hyperparameters
+    parser.add_argument('-lr', '--loss-rate', type=float, default=.001)
     args = parser.parse_args()
 
     cuda_wrapper = lambda x: x.cuda() if args.device == 'cuda' else x
     if not args.load_openi_model and not args.pretrained:
         model = resnet50(pretrained=False, num_classes=14)
-        if args.weight_init == 'kaiming':
-            model.apply(kaiming_init)
-        elif args.weight_init == 'xavier':
+        if args.weight_init == 'xavier':
             model.apply(xavier_init)
         model = cuda_wrapper(torch.nn.DataParallel(model))
     elif args.pretrained:
@@ -64,12 +56,12 @@ def main():
     else:
         train_loader, valid_loader, test_loader = get_guan_loaders(args.images_path, args.labels_path, args.batch_size, is_preprocessed=is_preprocessed, get_validation_set=True)
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=.01, momentum=.9, weight_decay=1e-4)
-    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=5, mode='min')
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.loss_rate, momentum=.9, weight_decay=1e-4)
+    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=5, mode='min')
     criterion = torch.nn.BCEWithLogitsLoss()
     reporting = Reporting(args.results_path)
     reporting.register(model, 'model', False)
-    runner = Resnet50RGBRun(
+    runner = RunModelWithAUCAndValLR(
         args,
         model,
         train_loader,
@@ -86,7 +78,7 @@ def main():
     del valid_loader
     torch.cuda.empty_cache()
     runner.generic_test_epoch()
-    reporting.save_all('resnet50-color')
+    reporting.save_all('resnet50-color-pretrained-{}'.format(args.pretrained))
 
 
 if __name__ == "__main__":
