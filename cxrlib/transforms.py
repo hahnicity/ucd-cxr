acdbcc9ead4a4dab593a99838bfeb65f59cb3147
Gregory Rehm <grehm87@gmail.com>
2018-07-28 01:20:23 -0700

    bug fixes for baltruschat loaders

--- cxrlib/transforms.py
+++ cxrlib/transforms.py
@@ -1,24 +1,22 @@
+from PIL.ImageOps import grayscale as to_grayscale
 import torch
 from torchvision import transforms
 
 
 class ToGrayscaleTensor(object):
     """
-    Converts a CXR image to a tensor and grayscale at once
+    Converts a CXR image to a tensor and grayscale
     """
     def __init__(self):
         self.to_tensor = transforms.ToTensor()
 
     def __call__(self, image):
-        image = image.convert('LA')
+        image = to_grayscale(image)
         image = self.to_tensor(image)
         # If we're converting to grayscale and there's a fourth channel
-        # that we don't want, then remove it
+        # then error out.
         if image.size(0) == 2 and (image[1] == 1).all():
-            image = image[0].view(1, image.size(1), image.size(2))
-        # I haven't seen this happen but there's a first time for everything
-        elif image.size(0) == 2 and not (image[1] == 1).all():
-            raise Exception('Tried to convert image to grayscale but non-1 information exists in 4 dimension')
+            raise Exception('Tried to convert image to grayscale but more than 1 channel exists!')
         return image
 
 
@@ -127,7 +125,7 @@ def five_crop_grayscale_transforms(norms):
         transforms.Resize(256),
         transforms.FiveCrop(224),
 		transforms.Lambda(
-			lambda crops: torch.stack([transforms.ToGrayscaleTensor()(crop) for crop in crops])
+			lambda crops: torch.stack([ToGrayscaleTensor()(crop) for crop in crops])
 		),
 		transforms.Lambda(
 			lambda crops: torch.stack([normalize(crop) for crop in crops])
@@ -136,7 +134,7 @@ def five_crop_grayscale_transforms(norms):
     test_transforms = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ToGrayscaleTensor(),
+        ToGrayscaleTensor(),
         normalize,
     ])
     return train_transforms, test_transforms
@@ -146,16 +144,17 @@ def baltruschat_grayscale_transforms(norms):
     normalize = transforms.Normalize(*norms)
     train_transforms = transforms.Compose([
         transforms.Resize(256),
-        transforms.CenterCrop(224),
+        # aspect ratio needs no change (3/4 to 4/3)
+        transforms.RandomResizedCrop(224, scale=(.7, 1.0)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(7),
-        transforms.ToGrayscaleTensor(),
+        ToGrayscaleTensor(),
         normalize,
     ])
-    train_transforms = transforms.Compose([
+    test_transforms = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ToGrayscaleTensor(),
+        ToGrayscaleTensor(),
         normalize,
     ])
     return train_transforms, test_transforms
@@ -165,13 +164,14 @@ def baltruschat_rgb_transforms(norms):
     normalize = transforms.Normalize(*norms)
     train_transforms = transforms.Compose([
         transforms.Resize(256),
-        transforms.CenterCrop(224),
+        # aspect ratio needs no change (3/4 to 4/3)
+        transforms.RandomResizedCrop(224, scale=(.7, 1.0)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(7),
         transforms.ToTensor(),
         normalize,
     ])
-    train_transforms = transforms.Compose([
+    test_transforms = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
